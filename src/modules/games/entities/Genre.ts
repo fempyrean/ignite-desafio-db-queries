a1b0d3c6e6878bc39e8d63b4982d2a480023777a
Lucas Sousa <frempyrean@gmail.com>
2021-03-27 13:04:23 -0300

    feat: add genre and order entities

---
+++ src/modules/games/entities/Genre.ts
@@ -0,0 +1,27 @@
+import {
+  Column,
+  Entity,
+  ManyToMany,
+  PrimaryGeneratedColumn,
+  CreateDateColumn,
+  UpdateDateColumn,
+} from "typeorm";
+import { Game } from "./Game";
+
+@Entity("genres")
+export class Genre {
+  @PrimaryGeneratedColumn("uuid")
+  id: string;
+
+  @Column()
+  title: string;
+
+  @ManyToMany(() => Game, (game) => game.genre)
+  games: Game[];
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  @UpdateDateColumn()
+  updated_at: Date;
+}
---
+++ src/modules/orders/entities/Order.ts
@@ -0,0 +1,23 @@
+import {
+  Entity,
+  ManyToMany,
+  PrimaryGeneratedColumn,
+  CreateDateColumn,
+  UpdateDateColumn,
+} from "typeorm";
+import { Game } from "../../games/entities/Game";
+
+@Entity("orders")
+export class Order {
+  @PrimaryGeneratedColumn("uuid")
+  id: string;
+
+  @ManyToMany(() => Game)
+  games: Game[];
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  @UpdateDateColumn()
+  updated_at: Date;
+}
